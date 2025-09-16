6277f4fdc2c40eef9ec18c3c3b13ac355b6ab912
sauravbag101 <sauravbag101@gmail.com>
2025-09-16 19:19:30 +0530

    b

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      // Ignore ESLint errors during builds (optional)
+      ignoreDuringBuilds: true,
+    },
+    experimental: {
+      typedRoutes: true,
+    },
+  }
+  
+  export default nextConfig;
+  
